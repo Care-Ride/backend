b848b337902e5c7f230746dfdeed5af5de12e3ce
jaehyyuun <jaehyunj393@gmail.com>
2025-11-14 03:31:28 +0900

    chore: initialize Spring Boot project

---
+++ src/test/java/backend/knowhow/KnowhowApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.knowhow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowhowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
