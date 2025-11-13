59290542027e7b69680512206bab458f87a4aaff
jaehyyuun <jaehyunj393@gmail.com>
2025-11-14 03:31:28 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/backend/knowhow/KnowhowApplication.java
@@ -0,0 +1,13 @@
+package backend.knowhow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowhowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowhowApplication.class, args);
+	}
+
+}
