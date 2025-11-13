8e37340b0abdfb1c2f92a0eb96e9109acf69a8cc
jaehyyuun <jaehyunj393@gmail.com>
2025-11-14 03:40:31 +0900

    feat: add Swagger configuration

---
+++ src/main/java/backend/knowhow/global/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package backend.knowhow.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CareRide API")
+                        .description("CareRide Backend API 명세서입니다.")
+                        .version("v1.0.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
