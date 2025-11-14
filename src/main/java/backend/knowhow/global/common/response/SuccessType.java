4cb85d33eb7bf612606ca31a4fc9cce611565c0c
jaehyyuun <jaehyunj393@gmail.com>
2025-11-15 00:40:33 +0900

    feat: add common api response

---
+++ src/main/java/backend/knowhow/global/common/response/SuccessType.java
@@ -0,0 +1,20 @@
+package backend.knowhow.global.common.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum SuccessType {
+
+    SUCCESS(HttpStatus.OK,"SUCCESS", "요청이 성공적으로 처리되었습니다."),
+    CREATED(HttpStatus.CREATED, "CREATED", "새 리소스가 생성되었습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+
+
+}
