62b8181fadacc74d8dedd9955d9b5fe553abe4e7
jaehyyuun <jaehyunj393@gmail.com>
2025-11-15 00:40:33 +0900

    feat: add common api response

---
+++ src/main/java/backend/knowhow/global/common/response/ApiResponse.java
@@ -0,0 +1,38 @@
+package backend.knowhow.global.common.response;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"status", "code", "message", "data"})
+public record ApiResponse<T>(
+        int status,
+        String code,
+        String message,
+        @JsonInclude(JsonInclude.Include.NON_NULL) T data
+) {
+    public static <T> ApiResponse<T> success(SuccessType success, T data) {
+        return new ApiResponse<>(
+                success.getHttpStatus().value(),
+                success.getCode(),
+                success.getMessage(),
+                data);
+    }
+
+    public static ApiResponse<Void> success(SuccessType success) {
+        return new ApiResponse<>(
+                success.getHttpStatus().value(),
+                success.getCode(),
+                success.getMessage(),
+                null);
+    }
+
+    public static ApiResponse<?> error(ErrorType error) {
+        return new ApiResponse<>(
+                error.getHttpStatus().value(),
+                error.getCode(),
+                error.getMessage(),
+                null);
+    }
+
+}
