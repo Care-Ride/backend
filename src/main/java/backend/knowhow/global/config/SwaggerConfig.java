a4e1de6a1124f60d199888973f01853589fe2a83
jaehyyuun <jaehyunj393@gmail.com>
2025-11-18 00:29:17 +0900

    feat: add Swagger JWT auth

--- src/main/java/backend/knowhow/global/config/SwaggerConfig.java
+++ src/main/java/backend/knowhow/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package backend.knowhow.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +20,16 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("CareRide API")
                         .description("CareRide Backend API 명세서입니다.")
-                        .version("v1.0.0"));
+                        .version("v1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("JWT",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("JWT"));
     }
 
     @Bean
