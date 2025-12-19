9f6239747bf761397ce456df8577e80cbdb39a1a
jettieb <bokjihee@naver.com>
2025-12-20 00:22:00 +0900

    fix: solve conflict error

--- src/main/java/backend/knowhow/domain/member/service/MemberDeviceSettingService.java
+++ src/main/java/backend/knowhow/domain/member/service/MemberDeviceSettingService.java
@@ -66,10 +66,4 @@ public class MemberDeviceSettingService {
     public boolean existsSettingByMember(Member member){
         return settingRepository.existsByMember(member);
     }
-
-    // 세팅 저장 유무
-    @Transactional(readOnly = true)
-    public boolean existsSettingByMember(Member member){
-        return settingRepository.existsByMember(member);
-    }
 }
