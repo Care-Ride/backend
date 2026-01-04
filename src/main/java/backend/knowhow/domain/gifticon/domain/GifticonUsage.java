de663585e064c1da428028210ea047a96b994dd5
jettieb <bokjihee@naver.com>
2026-01-05 04:50:06 +0900

    fix: add createdDate annotation in GifticonUsage entity

--- src/main/java/backend/knowhow/domain/gifticon/domain/GifticonUsage.java
+++ src/main/java/backend/knowhow/domain/gifticon/domain/GifticonUsage.java
@@ -4,6 +4,7 @@ import backend.knowhow.domain.member.domain.Member;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -40,6 +41,7 @@ public class GifticonUsage {
     @Column(nullable = false)
     private GifticonUsageStatus status;
 
+    @CreatedDate
     @Column(nullable = false)
     private LocalDateTime createdAt;
 }
