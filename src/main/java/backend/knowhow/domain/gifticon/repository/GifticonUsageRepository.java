771552d76ad2c7c7ef20cb9f45c4fce830dbdb4c
jettieb <bokjihee@naver.com>
2026-01-07 23:33:29 +0900

    feat: add gifticon barcode lookup

--- src/main/java/backend/knowhow/domain/gifticon/repository/GifticonUsageRepository.java
+++ src/main/java/backend/knowhow/domain/gifticon/repository/GifticonUsageRepository.java
@@ -1,5 +1,6 @@
 package backend.knowhow.domain.gifticon.repository;
 
+import backend.knowhow.domain.gifticon.domain.Gifticon;
 import backend.knowhow.domain.gifticon.domain.GifticonProduct;
 import backend.knowhow.domain.gifticon.domain.GifticonUsage;
 import backend.knowhow.domain.gifticon.domain.GifticonUsageStatus;
@@ -8,6 +9,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GifticonUsageRepository extends JpaRepository<GifticonUsage, Long> {
     Page<GifticonUsage> findAllByBuyerAndStatus(Member buyer, GifticonUsageStatus status, Pageable page);
+
+    Optional<GifticonUsage> findByGifticonAndBuyer(Gifticon gifticon, Member buyer);
 }
